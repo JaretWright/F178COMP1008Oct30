f71472098394ad06f91c8f043a08f5f8e08e8535
JaretWright <hkkA223>
2018-10-30 10:45:12 -0400

    Added my name to controller

--- src/f18comp1008oct30/CreatePersonViewController.java
+++ src/f18comp1008oct30/CreatePersonViewController.java
@@ -4,7 +4,8 @@ package f18comp1008oct30;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.Initializable;
-
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 /**
  * FXML Controller class
  *
@@ -12,12 +13,13 @@ import javafx.fxml.Initializable;
  */
 public class CreatePersonViewController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    @FXML
+    private Label nameLabel;
+
     @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+    public void initialize(URL location, ResourceBundle resources) {
+        nameLabel.setText("My name is Jaret");
+    }
+
 }
+ 
